e4ae7ea322f15c5c3e939f4790cadee1caa43643
Danilo Valente <danilovalente96@gmail.com>
2016-10-14 23:02:03 -0300

    Initial commit

---
+++ src/main/java/com/conpec/sade/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.conpec.sade.repository;
+
+import com.conpec.sade.domain.PersistentToken;
+import com.conpec.sade.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data MongoDB repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends MongoRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
---
+++ src/main/java/com/conpec/sade/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.conpec.sade.web.rest;
