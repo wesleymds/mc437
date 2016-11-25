1c98da18bc0e1d80fcd0dbfd332197653b835a60
Danilo Valente <danilovalente96@gmail.com>
2016-11-25 15:17:42 -0200

    Update Postgres credentials

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
+heroku git:remote -a mc-437
 heroku config:add java_opts='-Xmx128m -Xms128m -Xss256k -XX:+UseCompressedOops'
 heroku config:add JAVA_OPTS='-Xmx128m -Xms128m -Xss256k -XX:+UseCompressedOops'
