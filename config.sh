7ef95248dd9242dc8e6f30af8276000310b1b9f0
Danilo Valente <danilovalente96@gmail.com>
2016-11-25 13:21:26 -0200

    Create config script

---
+++ config.sh
@@ -0,0 +1,2 @@
+heroku config:add java_opts='-Xmx128m -Xms128m -Xss256k -XX:+UseCompressedOops'
+heroku config:add JAVA_OPTS='-Xmx128m -Xms128m -Xss256k -XX:+UseCompressedOops'
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+heroku pg:psql -c "update databasechangeloglock set locked=false;"
 ./gradlew -Pprod bootRepackage -x test
 heroku deploy:jar --jar build/libs/*war
