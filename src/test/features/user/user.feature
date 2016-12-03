8552f3c5e0faa8d45fe97602539e8a0eebe36e20
Allana De Macedo Idalgo <Allana De Macedo Idalgo>
2016-12-02 22:54:45 -0200

    Testes de mudar senha
    Signed-off-by: Allana De Macedo Idalgo <Allana De Macedo Idalgo>

--- src/test/features/user/user.feature
+++ src/test/features/user/user.feature
@@ -1,8 +1,14 @@
 Feature: User management
+    Background:
+        Given the page is open user "http://localhost:8080/#"
 
-	Scenario: Request change for user password
+	Scenario: Request change for user password and everything works
 		When I request to change my password
 		Then I receive an email with a link to change my password
+
+    Scenario: Request change for user password and the email is not regitered
+        When I request to change my password
+        Then I don  t receive an email with a link to change my password
 
     Scenario: Change user password
         When I click the link
@@ -15,7 +21,7 @@ Feature: User management
         And I click the link
         Then my account is confirmed
 
-    Scenario:
+    Scenario: C_O get an email
         When I register in the system
         And I confirm my account
         Then C_O get an email
--- src/test/java/br/com/conpec/sade/cucumber/stepdefs/UserStepDefs.java
+++ src/test/java/br/com/conpec/sade/cucumber/stepdefs/UserStepDefs.java
@@ -1,49 +1,171 @@
 package br.com.conpec.sade.cucumber.stepdefs;
 
-import br.com.conpec.sade.web.rest.UserDataResource;
+import br.com.conpec.sade.web.rest.AccountResource;
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
 
-import br.com.conpec.sade.web.rest.UserResource;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserStepDefs extends StepDefs {
+    private WebDriver webDriver;
 
     @Inject
-    private UserResource userResource;
-
+    private AccountResource accountResource;
     private MockMvc restUserMockMvc;
 
     @Before
     public void setup() {
-        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
+        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(accountResource).build();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");//Especificar caminho completo na máquina quando for testar
+
     }
 
-    @When("^I search user '(.*)'$")
-    public void i_search_user_admin(String userId) throws Throwable {
-        actions = restUserMockMvc.perform(get("/api/users/" + userId)
-                .accept(MediaType.APPLICATION_JSON));
+//    @Given("^the page is open \"([^\"]*)\"$")
+//    public void thePageIsOpen(String page) throws Throwable {
+//        webDriver = new ChromeDriver();
+//        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+//        webDriver.get(page);}
+
+    @When("^I request to change my password$")
+    public void iRequestToChangeMyPassword() throws Throwable {
+        webDriver.findElement(By.id("account-menu")).click();
+        webDriver.findElement(By.id("login")).click();
+        WebElement element = webDriver.findElement(By.id("forgot-password"));
+        Thread.sleep(5000);
+        element.click();
+        webDriver.findElement(By.id("email")).sendKeys("437test@gmail.com");
+        WebElement element2 = webDriver.findElement(By.id("reset-password"));
+        Thread.sleep(5000);
+        element2.click();
+        webDriver.close();
     }
 
-    @Then("^the user is found$")
-    public void the_user_is_found() throws Throwable {
+    @Then("^I receive an email with a link to change my password$")
+    public void iReceiveAnEmailWithALinkToChangeMyPassword() throws Throwable {
+        RequestPostProcessor postProcessor = new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.addHeader("Content-Type", "application/json;charset=UTF-8");
+                request.addHeader("Cookie", "XSRF-TOKEN=433821c0-5e27-47c6-b3fc-c30c38d1c370");
+                request.addHeader("X-XSRF-TOKEN", "433821c0-5e27-47c6-b3fc-c30c38d1c370");
+                return request;
+            }
+        };
+
+        actions = restUserMockMvc.perform(post("/api/account/reset_password/init").with(postProcessor)
+            .accept(MediaType.TEXT_PLAIN).contentType(MediaType.APPLICATION_JSON_UTF8).content("teste@teste"));
         actions
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+            .andExpect(status().isBadRequest());
     }
 
-    @Then("^his last name is '(.*)'$")
-    public void his_last_name_is(String lastName) throws Throwable {
-        actions.andExpect(jsonPath("$.lastName").value(lastName));
+
+
+    @When("^I click the link$")
+    public void iClickTheLink() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        webDriver.close();
+
+        throw new PendingException();
     }
 
+    @And("^I fill in the new password input and the confirmation input$")
+    public void iFillInTheNewPasswordInputAndTheConfirmationInput() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        webDriver.close();
+
+        throw new PendingException();
+    }
+
+    @Then("^my password is set to the new password$")
+    public void myPasswordIsSetToTheNewPassword() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        webDriver.close();
+
+        throw new PendingException();
+    }
+
+    @When("^I resgister in the system$")
+    public void iResgisterInTheSystem() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        webDriver.close();
+
+        throw new PendingException();
+    }
+
+    @And("^I get a confirmation email$")
+    public void iGetAConfirmationEmail() throws Throwable {
+        webDriver.close();
+// Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^my account is confirmed$")
+    public void myAccountIsConfirmed() throws Throwable {
+        webDriver.close();
+// Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I register in the system$")
+    public void iRegisterInTheSystem() throws Throwable {
+        webDriver.close();
+// Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I confirm my account$")
+    public void iConfirmMyAccount() throws Throwable {
+        webDriver.close();
+// Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^C_O get an email$")
+    public void c_oGetAnEmail() throws Throwable {
+
+        webDriver.close();
+// Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the page is open user \"([^\"]*)\"$")
+    public void thePageIsOpenUser(String arg0) throws Throwable {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        webDriver.get(arg0);
+    }
+
+    @Then("^I don  t receive an email with a link to change my password$")
+    public void iDonTReceiveAnEmailWithALinkToChangeMyPassword() throws Throwable {
+        RequestPostProcessor postProcessor = new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.addHeader("Content-Type", "application/json;charset=UTF-8");
+                request.addHeader("Cookie", "XSRF-TOKEN=433821c0-5e27-47c6-b3fc-c30c38d1c370");
+                request.addHeader("X-XSRF-TOKEN", "433821c0-5e27-47c6-b3fc-c30c38d1c370");
+                return request;
+            }
+        };
+        actions = restUserMockMvc.perform(post("/api/account/reset_password/init").with(postProcessor)
+            .accept(MediaType.TEXT_PLAIN).contentType(MediaType.APPLICATION_JSON_UTF8).content("errado@errado"));
+        actions
+            .andExpect(status().isBadRequest());
+    }
 }
